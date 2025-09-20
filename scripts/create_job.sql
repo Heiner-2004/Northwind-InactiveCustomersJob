043037f5d9dba307219808f1b1d2d7244eb16765
salometoroc-cmd <salome.toroc@udea.edu.co>
2025-09-20 08:59:32 -0500

    Update create_job.sql
    avance del script de JOB

--- scripts/create_job.sql
+++ scripts/create_job.sql
@@ -1,1 +1,17 @@
 
+--creo una tablaba para enteder 
+USE Northwind2025;
+GO
+IF NOT EXISTS (
+	SELECT 1
+	FROM INFORMATION_SCHEMA.TABLES
+	WHERE TABLE_NAME = 'InactiveCustomersLog'
+	)
+	
+--hay que hacer la clase InactiveCustomersLog
+BEGIN	
+	
+END
+
+-- insertar los cientes inactivos 
+INSERT INTO InactiveCustomersLog (CustomerID, CompanyName, ContactName, LastDate)
