55d4960eac082702ad10582d6a6561bd7dcb6396
AmyHerazo <amy.herazo@udea.edu.co>
2025-09-21 00:01:37 -0500

    Update create_inactive_log_table.sql

--- scripts/create_inactive_log_table.sql
+++ scripts/create_inactive_log_table.sql
@@ -1,1 +1,12 @@
 -- Script para crear la tabla de clientes inactivos 
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='InactiveCustomersLog' AND xtype='U')
+
+CREATE TABLE InactiveCustomersLog (
+    LogID INT IDENTITY(1,1) PRIMARY KEY,            
+    CustomerID NCHAR(5) NOT NULL,                   
+    CompanyName NVARCHAR(40) NOT NULL,              
+    ContactName NVARCHAR(30),                       
+    LastOrderDate DATE,                         
+    InactivityLoggedDate DATETIME DEFAULT GETDATE()        
+);
+
